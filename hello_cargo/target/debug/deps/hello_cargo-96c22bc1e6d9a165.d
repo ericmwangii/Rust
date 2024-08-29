ffc0d976d4400f7a0d0bd8fbdbb804be311cabc4
Eric Mwangi <ericmwangii1202@gmail.com>
2024-08-29 10:43:48 +0300

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-96c22bc1e6d9a165.d
@@ -0,0 +1,5 @@
+/home/eric/Rust/hello_cargo/target/debug/deps/hello_cargo-96c22bc1e6d9a165: src/main.rs
+
+/home/eric/Rust/hello_cargo/target/debug/deps/hello_cargo-96c22bc1e6d9a165.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/eric/Rust/hello_cargo/target/debug/hello_cargo: /home/eric/Rust/hello_cargo/src/main.rs
